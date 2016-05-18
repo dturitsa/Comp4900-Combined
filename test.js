f6019b2a5505a89f61b49fdd68a58a52d345234b
Austin Lott <ark.sky.dragon@gmail.com>
2016-05-18 14:47:44 -0700

    Finished better scaling for canvas elements.

--- test.js
+++ test.js
@@ -585,14 +585,14 @@ function ShowEditCanvas(element) {
 	var height = OrigCanvas.height;
 	canvas.width = width;
 	canvas.height = height;
-	console.log(width, height);
+	//console.log(width, height);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	$('#uploadedImage').imgAreaSelect({remove:true});
 	$("#ElementCanvas").css({"max-width": "100%" ,
-					"max-height":500 });
+					"max-height":300 });
 	$("#ElementDisplay").stop().animate({
-				width: (1000 + 10),
-				height: (1000 + 30),
+				width: 450 * 1.2,
+				height: 450,
 				left: pos.left, 
 				top: pos.top,
 				}).slideDown();
@@ -698,12 +698,14 @@ function drawCopiedImage(canvas, ev){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	//console.log(draggedElement);
 	//var longestSide = Math.max(draggedElement.width, draggedElement.height);
-	if(draggedElement.width >= draggedElement.height){
+	/*if(draggedElement.width >= draggedElement.height){
 	  ctx.drawImage(draggedElement, 0, 0, canvas.width, canvas.height * (draggedElement.height / draggedElement.width));
 	} else{
 	  ctx.drawImage(draggedElement, 0, 0, canvas.width * (draggedElement.width / draggedElement.height), canvas.height);
-	}
+	}*/
 	//console.log(canvas.width, canvas.height);
+
+	ctx.drawImage(draggedElement, 0, 0, canvas.width, canvas.height);
 }
 
 // loads the image and draws it on the canvas.
@@ -785,7 +787,7 @@ function getMousePosition2(e) { // NOTE*: These may need tweeking to work proper
 // and makes the selection
 
 function onMouseDown(e) {
-	console.log(erasing);
+	//console.log(erasing);
 	if(wandFlag || colorElimFlag) {
 	    if (e.button == 0) {
 	        allowDraw = true;
